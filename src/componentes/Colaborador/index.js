04829f68564de88a61ec44268a76a304cad111f3
dan.vsm <dan.vsm@gmail.com>
2023-08-29 21:57:20 -0300

    att

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,20 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/danvsm.png' alt='Daniel Santiago' />
+            </div>
+        
+            <div className='rodape'>
+                <h4>Daniel Santiago</h4>
+                <h5>Instrutor</h5>
+            </div>
+        
+        </div>
+       
+    )
+}
+
+export default Colaborador
